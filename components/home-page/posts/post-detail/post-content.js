94f2e30754fd8dff8d73c4690ed618189a08b3f7
alperen.yildirim <alperen.yildirim@ktb.gov.tr>
2022-12-23 10:12:39 +0300

    a

--- components/home-page/posts/post-detail/post-content.js
+++ components/home-page/posts/post-detail/post-content.js
@@ -1,3 +1,4 @@
+import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 const DUMMY_POST =
@@ -14,7 +15,7 @@ function PostContent() {
     return (
         <article className={classes.content}>
             <PostHeader title={DUMMY_POST.title} image={imagePath}/>
-            {DUMMY_POST.content}
+         <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
         </article>
     )
 }
