d48bdf9b5deb3d670401d7f85ce14a7b34fb8edc
alperen.yildirim <alperen.yildirim@ktb.gov.tr>
2022-12-23 10:10:59 +0300

    a

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,4 +1,9 @@
+import PostContent from "../../components/home-page/posts/post-detail/post-content";
+
 function PostDetailPage() {
+    return(
+        <PostContent />
+    )
 }
 
 export default PostDetailPage;
