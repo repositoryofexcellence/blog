4c34e6f8d8c29dc0f81049b71566c9b7438d6d0f
alperen.yildirim <alperen.yildirim@ktb.gov.tr>
2023-01-03 16:31:50 +0300

    a

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,6 @@ import LayoutClient from "../components/client/layout/layout";
 import LayoutAdmin from "../components/admin/layout/layout";
 
 export default function App({Component, pageProps, router}) {
-    console.log(router.route)
-
-
     return (
         router.route.includes('yonetim-paneli') ?
             <LayoutAdmin>
